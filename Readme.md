b39dec32e35fa53855a8322857e47d75413fe08a
Toby Lausch <toby@lauscht.com>
2020-06-01 18:11:40 +0200

    Added Summary to linter.

--- Readme.md
+++ Readme.md
@@ -1,4 +1,9 @@
 # Githooks
 
-A minor collection of useful git hooks.
+In order to evaluate your python style consider a least pylint rating.
+To do so, e.g. install the following git pre-commit hook to enable clean code.
+
+    .git/hook/pre-commint
+    #!/bin/sh
+    python -m githooks.linter -warn --minimum 10
 
--- githooks/linter.py
+++ githooks/linter.py
@@ -1,5 +1,6 @@
 import os
 import re
+import logging
 import argparse
 from githooks.execution import Exec
 from githooks.gitwrap import Git
@@ -28,25 +29,41 @@ class Linter:
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('minimum_rating', default=10, nargs='?')
+    parser.add_argument('-minimum', default=9, nargs='?')
+    parser.add_argument('--warn', action='store_true')
     parser.add_argument('-v', action='store_true')
+    parser.add_argument('--disable_summary', action='store_true')
+
     args = parser.parse_args()
+    least_expected_rating = args.minimum
 
     git = Git()
-    print(git.last_commit())
-    print(git.diff_index())
     files = git.get_staged_modules()
     linter = Linter()
 
+    ratings = []
     for file in files:
         if not os.path.exists(file):
+            logging.warning("Missing file {file}".format(file=file))
             continue
+
         report = linter.create_report(file)
         rating = linter.extract_rating(report)
-        if args.minimum_rating > rating:
-            print(f"{file}: {rating}")
+        ratings.append((rating))
+        if rating < least_expected_rating:
+            if args.warn or args.v:
+                print("{file}: {rating}".format(file=file, rating=rating))
             if args.v:
                 print(report)
+
+    if not args.disable_summary:
+        print("Your code was rated in average {avg:.2f}/10 with at least {min:.2f}/10".format(
+            avg=(sum(ratings) / len(ratings)), min=min(ratings)
+        ))
+
+    if any(r < least_expected_rating for r in ratings):
+        return -1
+    return 0
 
 
 if __name__ == '__main__':
